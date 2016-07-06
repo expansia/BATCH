df81e367d41b10fd8465345600094ce8915a67ec
expansia <stagiaire_etn.ara@pcas.com>
2016-07-06 09:56:24 +0200

    Format ANSI du fichier + définition de variables pour réutiliser la fonction de renommage.

--- renomme_sysprep.cmd
+++ renomme_sysprep.cmd
@@ -1,4 +1,4 @@
-CLS REM On efface l'â€šcran
+CLS REM On efface l'‚cran
 @echo off
 echo +-----------------------------------------------------------------------------+
 echo ^| Fichier     : renomme_sysprep.bat                                           ^|
@@ -6,29 +6,35 @@ echo ^| Version     : 1                                                         
 echo ^| Auteur      : Bruno Boissonnet                                              ^|
 echo ^| Date        : 24/06/2016                                                    ^|
 echo ^| Description : Renomme le fichier sysprep.bat en sysprep.bat.old.            ^|
-echo ^| Remarques   : Le fichier doit Ë†tre sauvegardâ€š au format ANSI.               ^|
+echo ^| Remarques   : Le fichier doit …tre sauvegard‚ au format ANSI.               ^|
 echo ^|                                                                             ^|
 echo +-----------------------------------------------------------------------------+
 echo(
 echo Faire Ctrl+C pour quitter le programme ou appuyer sur une touche pour continuer.
 
-REM On fait une pause pour que la fenÃªtre reste affichÃ©e
+REM On fait une pause pour que la fenêtre reste affichée
 pause>nul
 
 REM +--DEBUT-DU-PROGRAMME---------------------------------------------------------+
 
-:: Renomme le fichier sysprep.bat en sysprep.bat.old.
+:: Définir le nom d'origine et le nom final
+:: ATTENTION, le nom d'origine doit contenir le chemin complet
 
-ren d:\INFORMATIQUE\sysprep.bat sysprep.bat.old
+SET NOM_FICHIER_ORIGINE=d:\INFORMATIQUE\sysprep.bat
+SET NOM_FICHIER_FINAL=sysprep.bat.old
+
+:: Renomme le fichier.
+
+ren %NOM_FICHIER_ORIGINE% %NOM_FICHIER_FINAL%
 
 REM +--FIN-DU-PROGRAMME-----------------------------------------------------------+
 
 echo(
-REM On fait une pause pour que la fenÃªtre reste affichÃ©e
+REM On fait une pause pour que la fen…tre reste affich‚e
 echo Appuyer sur une touche pour quitter le programme.
 pause>nul
 
 REM +--ACCENTS-EN-CODEPAGE-850----------------------------------------------------+
-REM echo "Affichage des caractÅ res diacritiques franâ€¡ais au format console :"
-REM  "Ã© Ã¨ Ã§ Ã  Ã¢ Ãª Ã® Ã¹"
-REM echo "â€š Å  â€¡ â€¦ Æ’ Ë† Å’ â€”"
+REM echo "Affichage des caractŠres diacritiques fran‡ais au format console :"
+REM  "é è ç à â ê î ù"
+REM echo "‚ Š ‡ … ƒ ˆ Œ —"
