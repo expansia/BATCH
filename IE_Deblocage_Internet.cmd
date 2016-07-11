5a40156929b46327a9b223d71416e5e74eadb9fb
expansia <stagiaire_etn.ara@pcas.com>
2016-07-11 13:08:01 +0200

    Version 2.0. MAJ du format

--- IE_Deblocage_Internet.cmd
+++ IE_Deblocage_Internet.cmd
@@ -1,5 +1,25 @@
-echo OFF
-REM Supprime les clés du registre qui permette d'empêcher la connexion internet.
+CLS REM On efface l'‚cran
+@echo off
+echo +-----------------------------------------------------------------------------+
+echo ^| Fichier     : IE_Deblocage_Internet.cmd                                     ^|
+echo ^| Version     : 0.2                                                           ^|
+echo ^| Auteur      : Bruno Boissonnet                                              ^|
+echo ^| Date        : 11/07/2016                                                    ^|
+echo ^| Description : Supprime les cl‚s du regsitre qui empˆchent la connexion      ^|
+echo ^|             : … internet.                                                   ^|
+echo ^| Remarques   : Le fichier doit ˆtre sauvegard‚ au format ANSI.               ^|
+echo +-----------------------------------------------------------------------------+
+echo(
+echo Faire Ctrl+C pour quitter le programme ou appuyer sur une touche pour continuer.
+
+REM On fait une pause pour que la fenêtre reste affichée
+pause>nul
+
+REM +--DEBUT-DU-PROGRAMME---------------------------------------------------------+
+
+REM echo "Affichage des caractŠres diacritiques fran‡ais au format ANSI :"
+REM  "é è ç à â ê î ù"
+REM echo "‚ Š ‡ … ƒ ˆ Œ —"
 
 
 echo ----------------------------------------------------------------- >> d:\exp_policies.log
@@ -19,3 +39,12 @@ echo %DATE% %TIME% Ajout de la clef ProxyEnable : %errorlevel% >> d:\exp_policie
 
 reg delete "HKCU\Software\Policies\Microsoft\Internet Explorer\Control Panel" /v ConnectionsTab /f
 echo %DATE% %TIME% Suppression de la clef ConnectionsTab : %errorlevel% >> d:\exp_policies.log
+
+
+REM +--FIN-DU-PROGRAMME-----------------------------------------------------------+
+
+echo(
+REM On fait une pause pour que la fenêtre reste affichée
+echo Appuyer sur une touche pour quitter le programme.
+pause>nul
+
