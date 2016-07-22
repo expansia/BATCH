b5efaca045c068f762ed056079cce173049658fe
expansia <stagiaire_etn.ara@pcas.com>
2016-07-22 22:19:59 +0200

    Version 2.1. Ajout de commentaires expliquant la commande REG.

--- IE_Deblocage_Internet.cmd
+++ IE_Deblocage_Internet.cmd
@@ -2,7 +2,7 @@ CLS REM On efface l'‚cran
 @echo off
 echo +-----------------------------------------------------------------------------+
 echo ^| Fichier     : IE_Deblocage_Internet.cmd                                     ^|
-echo ^| Version     : 2.0                                                           ^|
+echo ^| Version     : 2.1                                                           ^|
 echo ^| Auteur      : Bruno Boissonnet                                              ^|
 echo ^| Date        : 11/07/2016                                                    ^|
 echo ^| Description : Supprime les cl‚s du regsitre qui empˆchent la connexion      ^|
@@ -23,6 +23,26 @@ REM echo "‚ Š ‡ … ƒ ˆ Œ —"
 
 
 echo ----------------------------------------------------------------- >> d:\exp_policies.log
+
+REM ------------------------------------------------------------------
+
+REM REG DELETE Nom_de_clé [/v Nom_de_valeur | /ve| /va] [/f]
+REM Supprime la clé du registre passée en paramètre
+REM  /f          Force la suppression sans demander de confirmation.
+
+REM -----------------------------------------------------------------
+
+REM REG ADD Nom_de_clé [/v Nom_de_valeur | /ve] [/t Type] [/s Séparateur]
+                   [/d Données] [/f]
+REM Crée une clé dans le registre
+REM  /v       Nom de la valeur, sous la clé sélectionnée, à ajouter.
+REM  /t       RegKey types de données
+REM           [ REG_SZ    | REG_MULTI_SZ | REG_EXPAND_SZ |
+REM             REG_DWORD | REG_QWORD    | REG_BINARY    | REG_NONE ]
+REM           Si ce paramètre est omis, REG_SZ est pris par défaut.
+REM  /d       Données à affecter au Nom_de_valeur ajouté
+REM  /f       Force l'écrasement de l'entrée de Registre existante, sans
+           confirmation.
 
 
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /f
