3eb8514b59c67f46881575b53ff6e0d6f995c3ad
expansia <stagiaire_etn.ara@pcas.com>
2016-08-24 21:50:06 +0200

    Suppression des caractères cp850 + ajout de remarques et commentaires + utilisation de variables.

--- Sauvegarde_Manuelle_Sur_ARAMON02.cmd
+++ Sauvegarde_Manuelle_Sur_ARAMON02.cmd
@@ -2,13 +2,14 @@ CLS REM On efface l'‚cran
 @echo off
 echo +-----------------------------------------------------------------------------+
 echo ^| Fichier     : Sauvegarde_Manuelle_Sur_ARAMON02.cmd                          ^|
-echo ^| Version     : 0.1                                                           ^|
+echo ^| Version     : 0.2                                                           ^|
 echo ^| Auteur      : Bruno Boissonnet                                              ^|
 echo ^| Date        : 15/07/2015                                                    ^|
 echo ^| Description : Copie la sauvegarde de DECA sur ARAMON02.                     ^|
-echo ^| Remarques   : Le fichier doit ˆtre sauvegard‚ au format ANSI.               ^|
-echo ^|             : dossier source : D:\SaveDECA\.                                ^|
-echo ^|             : dossier destination : \\Aramon02\e$\SAUVEGARDES\DECAv7.10.32. ^|
+echo ^| Remarques   : Ce script est lanc, par une t‡che planifi,e.                  ^|
+echo ^|               Dossier source : D:\SaveDECA\.                                ^|
+echo ^|               Dossier destination : \\Aramon02\e$\SAUVEGARDES\DECAv7.10.32. ^|
+echo ^|               Le fichier doit ˆtre sauvegard‚ dans l'encodage ANSI(cp1252). ^|
 echo +-----------------------------------------------------------------------------+
 echo(
 REM echo Faire Ctrl+C pour quitter le programme ou appuyer sur une touche pour continuer.
@@ -18,18 +19,20 @@ REM pause>nul
 
 REM +--DEBUT-DU-PROGRAMME---------------------------------------------------------+
 
-echo "Affichage des caractŠres diacritiques fran‡ais au format ANSI :"
-REM  "é è ç à â ê î ù"
-REM echo "‚ Š ‡ … ƒ ˆ Œ —"
 
-echo ***** Copie le dossier D:\SaveDECA vers \\Aramon02\e$\SAUVEGARDES\DECAv7.10.32
+SET SOURCE=D:\SaveDECA\
+SET DESTINATION=\\Aramon02\e$\SAUVEGARDES\DECAv7.10.32
+
+
+echo ***** Copie le dossier %SOURCE% vers %DESTINATION%
 REM Paramètres de xcopy
 REM /Y : Supprime la demande de confirmation de remplacement de fichiers de destination existants.
 REM /E : Copie les répertoires et sous-répertoires, y compris les répertoires vides.
 REM /V : Vérifie chaque nouveau fichier.
 
-xcopy D:\SaveDECA\*.*  \\Aramon02\e$\SAUVEGARDES\DECAv7.10.32 /Y/E/V
-%errorlevel%
+xcopy %SOURCE%*.*  %DESTINATION% /Y/E/V
+
+%errorlevel% REM on renvoie le code d'erreur car le r,sultat du script va dans un fichier log.
 
 
 REM +--FIN-DU-PROGRAMME-----------------------------------------------------------+
