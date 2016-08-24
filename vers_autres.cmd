01f2c0aebe0fabe7addab2021ef905b0c012e3ed
expansia <stagiaire_etn.ara@pcas.com>
2016-08-24 10:21:20 +0200

    1) Suppression de la traduction en cp850 des caractères diacritiques. 2) Modification de la façon de récupérer les réponses de l'utilisateur :
    	- ENTRER pour valider.
    	- N'importe quelle touche et ENTRER pour modifier la saisie.
    	- Ctrl+C pour quitter le programme.
    
    3) Récupération de la date au format long (avec	powershell) pour afficher une date plus lisible à l'utilisateur.
    4) Récupération de la date au format français court.
    5) Transformation de la date du format français	court vers le format demandé par XCOPY.
    6) On efface l'écran avant chaque étape du programme.

--- vers_autres.cmd
+++ vers_autres.cmd
@@ -1,10 +1,10 @@
-CLS REM On efface l'‚cran
+CLS :: On efface l'‚cran
 @echo off
 echo +-----------------------------------------------------------------------------+
 echo ^| Fichier     : vers_autres.cmd                                               ^|
-echo ^| Version     : 0.2                                                           ^|
+echo ^| Version     : 3.0                                                           ^|
 echo ^| Auteur      : Bruno Boissonnet                                              ^|
-echo ^| Date        : 17/07/2015                                                    ^|
+echo ^| Date        : 07/08/2016                                                    ^|
 echo ^| Description : Copie l'historique du serveur SRVLIN1 vers un autre poste.    ^|
 echo ^| Remarques   : Le fichier doit ˆtre sauvegard‚ au format ANSI.               ^|
 echo ^|             : 1) Copie tous les fichiers *.idx et *.lgh du dossier          ^|
@@ -14,64 +14,75 @@ echo +--------------------------------------------------------------------------
 echo(
 echo Faire Ctrl+C pour quitter le programme ou appuyer sur une touche pour continuer.
 
-REM On fait une pause pour que la fenêtre reste affichée
+:: On fait une pause pour que la fenêtre reste affichée
 pause>nul
 
-REM +--DEBUT-DU-PROGRAMME----------------------------------------------------------+
-
-REM echo "Affichage des caractŠres diacritiques fran‡ais au format ANSI :"
-REM  "é è ç à â ê î ù"
-REM echo "‚ Š ‡ … ƒ ˆ Œ —"
+:: +--DEBUT-DU-PROGRAMME----------------------------------------------------------+
 
 
-
-REM +--SUPPRESION DES ANCIENS FICHIERS IDX ET LGH----------------------------------+
+:: +--SUPPRESION DES ANCIENS FICHIERS IDX ET LGH----------------------------------+
 
 echo(
 echo ***** 1) Suppresion des anciens fichiers idx et lgh
-REM Paramètres de del
-REM /Q : Pas de confirmation avant suppression avec car. génériques.
+echo(
+:: Paramètres de del
+:: /Q : Pas de confirmation avant suppression avec car. génériques.
 
 del /Q E:\vers_autres\*.idx
 del /Q E:\vers_autres\*.lgh
 
 
-REM +--RECUPERATION-DES-FICHIERS-IDX-ET-LGH----------------------------------------+
+
+:: +--RECUPERATION-DES-FICHIERS-IDX-ET-LGH----------------------------------------+
+
+:recuperation_des_fichiers
+CLS :: On efface l'‚cran
+echo(
+echo ***** 2) R‚cupŠration des fichiers de D:\Expansia\Histo\ dans E:\vers_autres\
+echo(
+:: Paramètres de xcopy
+:: /Y : Supprime la demande de confirmation de remplacement de fichiers de destination existants.
+:: /E : Copie les répertoires et sous-répertoires, y compris les répertoires vides.
+:: /V : Vérifie chaque nouveau fichier.
+:: /d[:mm-dd-yyyy] : Copies source files changed on or after the specified date only.
+::                   If you do not include a mm-dd-yyyy value, xcopy copies all Source
+::                   files that are newer than existing Destination files. This command-line
+::                   option allows you to update files that have changed.
+
+
+
+set /p UserInputPath= Entrer la date … partir de laquelle r‚cup‚rer les fichiers d'historique (au format jj/mm/aaaa)
 
 echo(
-echo ***** 2) R‚cupŠration des fichiers de D:\Expansia\Histo\
-REM Paramètres de xcopy
-REM /Y : Supprime la demande de confirmation de remplacement de fichiers de destination existants.
-REM /E : Copie les répertoires et sous-répertoires, y compris les répertoires vides.
-REM /V : Vérifie chaque nouveau fichier.
-REM /d[:mm-dd-yyyy] : Copies source files changed on or after the specified date only.
-REM                   If you do not include a mm-dd-yyyy value, xcopy copies all Source
-REM                   files that are newer than existing Destination files. This command-line
-REM                   option allows you to update files that have changed.
+:: R‚cupŠration de la date au format long (jour Num mois ann‚e)
+for /f "delims=" %%a in ('powershell -Command "Get-Date -Date %UserInputPath% -format D"') do @set DATE_LONGUE=%%a
 
-
-
-set /p UserInputPath= Entrer la date … partir de laquelle r‚cup‚rer les fichiers d'historique (au format mm-jj-aaaa)
-
-echo(
 echo Vous avez tap‚ : %UserInputPath%
-echo Si la date est correcte (mois-jour-ann‚e), appuyer sur une touche.
-echo Tous les fichiers plus r‚cents que cette date (%UserInputPath%) seront r‚cup‚r‚s.
-echo Sinon faire Ctrl+C pour quitter le programme.
-pause>nul
-echo(
+echo Tous les fichiers plus r‚cents que le %DATE_LONGUE% seront r‚cup‚r‚s.
+set ma_saisie=
+set /p ma_saisie=Appuyer sur ENTRER pour valider ou sur toute autre touche puis ENTRER pour pour modifier la date. Faire Ctrl+C pour quitter le programme.
+
+if DEFINED ma_saisie goto recuperation_des_fichiers :: Si on appuie sur ENTRER seulement, ma_saisie n'est pas définie
+
+:: On met la date au format exig, par XCOPY (mm/jj/aaaa)
+SET DATE_RECUP=%UserInputPath:~3,2%-%UserInputPath:~0,2%-%UserInputPath:~6,4%
+echo %DATE_RECUP%
+
+:: R‚cupŠration des fichiers dans E:\vers_autres\
+xcopy D:\Expansia\Histo\*.idx E:\vers_autres\ /d:%DATE_RECUP%
+xcopy D:\Expansia\Histo\*.lgh E:\vers_autres\ /d:%DATE_RECUP%
 
 
-xcopy D:\Expansia\Histo\*.idx E:\vers_autres\ /d:%UserInputPath%
-xcopy D:\Expansia\Histo\*.lgh E:\vers_autres\ /d:%UserInputPath%
 
 
-REM +--SELECTION-DU-POSTE----------------------------------------------------------+
+:: +--SELECTION-DU-POSTE----------------------------------------------------------+
 
-CLS REM On efface l'‚cran
+:selection_du_poste
+
+CLS :: On efface l'‚cran
 echo(
 echo ***** 2) S‚lection du poste … r‚cup‚rer.
-REM:debut
+echo(
 
 echo a- Copier sur OP_01
 echo b- Copier sur OP_02
@@ -83,8 +94,9 @@ echo g- Copier sur OP_07
 echo h- Copier sur OP_08
 echo i- Copier sur SRVLIN2
 echo j- Copier sur REPLI
+echo(
 
-CHOICE /C abcdefghij /N /T 90 /D a /M "Choisissez le poste … r‚cup‚rer."
+CHOICE /C abcdefghij /N /T 90 /D a /M "Choisissez le poste … r‚cup‚rer en tapant la lettre correspondante (ex: a pour OP_01)."
 
 if errorlevel 10 SET POSTE=REPLI
 if errorlevel 9 SET POSTE=SRVLIN2
@@ -99,28 +111,31 @@ if errorlevel 1 SET POSTE=OP_01
 
 echo(
 echo Vous avez tap‚ : %POSTE%
-echo Si le nom du poste est correct, appuyer sur une touche.
-echo Sinon faire Ctrl+C pour quitter le programme.
-pause>nul
+set ma_saisie=
+set /p ma_saisie=Appuyer sur ENTRER pour valider ou sur toute autre touche puis ENTRER pour pour modifier le nom du poste. Faire Ctrl+C pour quitter le programme.
+::set /p ma_saisie=Si le nom du poste est correct, tapez O (Oui), sinon tapez N (Non). Faire Ctrl+C pour quitter le programme.
+
+if DEFINED ma_saisie goto selection_du_poste :: Si on appuie sur ENTRER seulement, ma_saisie n'est pas définie
 
 
-REM +--COPIE-DES-FICHIERS-SUR-LE-POSTE---------------------------------------------+
+:: +--COPIE-DES-FICHIERS-SUR-LE-POSTE---------------------------------------------+
 
 echo(
-echo Copie vers %POSTE% : BAT01
-echo =========================
+echo Copie vers %POSTE%
+echo ==================
+echo(
 xcopy E:\vers_autres\*.idx \\%POSTE%\d$\EXPANSIA\Histo\ /Y
 xcopy E:\vers_autres\*.lgh \\%POSTE%\d$\EXPANSIA\Histo\ /Y
 
-REM On fait les tests dans un r‚pertoire temporaire d:\test_bruno sur le poste
+:: On fait les tests dans un r‚pertoire temporaire d:\test_bruno sur le poste
 REM xcopy E:\vers_autres\*.idx \\%POSTE%\d$\test_bruno\ /Y
 REM xcopy E:\vers_autres\*.lgh \\%POSTE%\d$\test_bruno\ /Y
 
 
-REM +--FIN-DU-PROGRAMME-----------------------------------------------------------+
+:: +--FIN-DU-PROGRAMME-----------------------------------------------------------+
 
 echo(
-REM On fait une pause pour que la fenêtre reste affichée
+:: On fait une pause pour que la fenêtre reste affichée
 echo Appuyer sur une touche pour quitter le programme.
 pause>nul 
 exit
