651db2df5f3b84459ea7151fb0d100953a33f79d
expansia <stagiaire_etn.ara@pcas.com>
2016-08-24 12:01:33 +0200

    v3.0.Ajout d'un modèle, ajout de paramètres à la commande copy, test de la variable de retour.

--- installation_Fra32_xml.bat
+++ installation_Fra32_xml.bat
@@ -1,26 +1,66 @@
-REM Installation de Granta sur un poste client
-REM Bruno Boissonnet
-REM 28/10/2014
+CLS REM On efface l'‚cran
+@echo off
+echo +-----------------------------------------------------------------------------+
+echo ^| Fichier     : Installation_FRA32_XML.bat                                    ^|
+echo ^| Version     : 3.0                                                           ^|
+echo ^| Auteur      : Bruno Boissonnet                                              ^|
+echo ^| Date        : 06/08/2016                                                    ^|
+echo ^| Description : Installation de Granta sur un poste client.                   ^|
+echo ^|                                                                             ^|
+echo ^| Remarques   : Le fichier doit ˆtre sauvegard‚ dans l'encodage ANSI(cp1252). ^|
+echo ^|               Le fichier doit ˆtre lanc‚ en MODE ADMINISTRATEUR.            ^|
+echo ^|               Copie le fichier Fra32.XML dans le dossier adequat            ^|
+echo ^|               afin d'avoir les informations "Point de Rassemblement",       ^|
+echo ^|               "Matricule" etc ... dans l'onglet "Renseignements Personnels" ^|
+echo ^|               du porteur de badge.                                          ^|
+echo +-----------------------------------------------------------------------------+
+echo(
+echo Faire Ctrl+C pour quitter le programme ou appuyer sur une touche pour continuer.
 
-REM Copie le fichier Fra32.XML dans le dossier adequat
-REM Afin d'avoir les informations "Point de Rassemblement",
-REM "Matricule" etc ... dans l'onglet "Renseignements Personnels"
-REM du porteur de badge.
-
-set DOSSIER_DESTINATION="C:\sysmgrsql\progs\"
-set FICHIER="Fra32.XML"
+REM On fait une pause pour que la fenêtre reste affichée
+pause>nul
 
 
-REM On se met dans le rÃ©pertoire du script
-pushd "%~dp0"
 
-REM On crÃ©e le dossier de destination s'il n'existe pas (debug)
-if not EXIST %DOSSIER_DESTINATION% (
-	mkdir %DOSSIER_DESTINATION%
+
+REM +--DEBUT-DU-PROGRAMME---------------------------------------------------------+
+
+
+SET SOURCE=Fra32.XML
+SET DESTINATION=C:\sysmgrsql\progs\
+
+
+:: Va dans le dossier du script (pour trouver le fichier source)
+pushd %~dp0
+
+
+:: Copie le fichier dans le dossier "etc"
+copy /Y /V %SOURCE% %DESTINATION% > nul
+
+:: Test pour afficher la ligne de commande : OK
+:: echo On
+:: copy /Y /V hosts "C:\Windows\System32\drivers\etc\" > nul & echo off
+:: echo off
+
+
+echo(
+
+if %ERRORLEVEL% == 0 (
+	echo SUCCÔS. Fichier %SOURCE% copi‚ dans %DESTINATION% !
+) else (
+	echo ERREUR. Le fichier %SOURCE% n'a pas ‚t‚ copi‚ dans %DESTINATION%.
 )
 
-REM On copie le fichier dans le dossier
-copy %FICHIER% %DOSSIER_DESTINATION%
+echo(
 
-REM On se remet dans le rÃ©pertoire d'origine
+:: revient dans le dossier initial
 popd
+
+
+
+REM +--FIN-DU-PROGRAMME-----------------------------------------------------------+
+
+echo(
+REM On fait une pause pour que la fenêtre reste affichée
+echo Appuyer sur une touche pour quitter le programme.
+pause>nul 
