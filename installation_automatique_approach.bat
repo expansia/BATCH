07418e2ecc50493f25992a53ef75eedbb7b4357f
expansia <stagiaire_etn.ara@pcas.com>
2016-08-29 22:33:08 +0200

    V3.0. Ajout du modèle pour fichier batch + utilisation de pushd-popd + message de fin de script.

--- installation_automatique_approach.bat
+++ installation_automatique_approach.bat
@@ -1,29 +1,47 @@
+CLS REM On efface l'‚cran
 @echo off
+echo +-----------------------------------------------------------------------------+
+echo ^| Fichier     : installation_automatique_approach.bat                         ^|
+echo ^| Version     : 3.0                                                           ^|
+echo ^| Auteur      : Bruno Boissonnet                                              ^|
+echo ^| Date        : 30/10/2014                                                    ^|
+echo ^| Description : Lance une installation automatique silencieuse de Approach.   ^|
+echo ^|                                                                             ^|
+echo ^| Remarques   : - Comme c'est une installation silencieuse, on ne voit rien.  ^|
+echo ^|               Il faut donc laisser 5 à 6 min que l'installation se termine. ^|
+echo ^|               - Installation pour tous les utilisateurs.                    ^|
+echo ^|               - Répertoire d'installation : c:\lotus.                       ^|
+echo ^|               - Utilisateur : EXPANSIA - Entreprise : EXPANSIA.             ^|
+echo ^|               - Voir le fichier instuit.rsp pour plus d'explications.       ^|
+echo +-----------------------------------------------------------------------------+
+::echo(
+::echo Faire Ctrl+C pour quitter le programme ou appuyer sur une touche pour continuer.
 
-REM Description : Lance une installation automatique silencieuse de Approach
-REM Auteur : Bruno Boissonnet
-REM Date : 30/10/2014
-REM Remarques :
-REM   - Comme c'est une installation silencieuse, on ne voit rien.
-REM     Il faut donc laisser 5 à 6 minutes que l'installation se termine.
-REM   - Installation pour tous les utilisateurs.
-REM   - Répertoire d'installation : c:\lotus.
-REM   - Utilisateur : EXPANSIA.
-REM   - Entreprise : EXPANSIA.
-REM   - Voir le fichier instuit.rsp pour plus d'explications.
- 
+REM On fait une pause pour que la fenˆtre reste affich‚e
+::pause>nul
 
-REM Change de disque si nécessaire
-%~d0
+
+REM +--DEBUT-DU-PROGRAMME---------------------------------------------------------+
 
 
 REM On se place dans le répertoire du script qui est le répertoire
 REM du fichier setup et du fichier instuit.rsp
 REM %0 est le nom du fichier batch. ~dp renvoie le disque et le répertoire de l'argument spécifié.
 
-cd "%~dp0"
-
+pushd %~dp0
 
 REM On lance l'installation automatisée avec le fichier de réponse
 
 setup /v"/qn RSP=\"%~dp0\instsuit.rsp\""
+
+:: revient dans le dossier initial
+popd
+
+
+
+REM +--FIN-DU-PROGRAMME-----------------------------------------------------------+
+:END
+echo(
+REM On fait une pause pour que la fenˆtre reste affich‚e
+echo Appuyer sur une touche pour quitter le programme.
+pause>nul 
